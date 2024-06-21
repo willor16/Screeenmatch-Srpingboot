1606ec965773f9a07b9654d6aa1f85ddb96107ab
wilmer_choxom <lorwilm@gmail.com>
2024-06-20 22:13:11 -0600

    iniciando proyecto

---
+++ src/test/java/com/Wilmer/Screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wilmer.Screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
